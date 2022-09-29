4d95988150e57a60e017387fe817d2dd9416dfe6
Anju0307 <jpmantilla01@misena.edu.co>
2022-09-29 18:32:16 -0500

    Ejercicio 9

--- main.js
+++ main.js
@@ -2,19 +2,18 @@ addEventListener("DOMContentLoaded", (e)=>{
     let form = document.querySelector("#form");
     form.addEventListener("submit" , (e)=>{
         e.preventDefault();
-        let numeros = document.querySelector("#numeros").value;
-        let numero = parseInt(prompt(`Ingrese el numero 1`));
-        let suma=numero,resta=numero,multi=numero,divi=numero;
-        for (let i=1; i<numeros;i++){
-            let numero2 = parseInt(prompt(`Ingrese el numero ${i+1}`));
-            suma += numero2;
-            resta -= numero2;
-            multi *= numero2;
-            divi /= numero2;
+        const iva=0.16,descuento=0.15,precio = 100000;
+        let total=0;
+        let cantidad = document.querySelector("#cantitad").value;
+        for (let i=1; i<=cantidad;i++){
+            total+=precio;
         }
-        alert(`El resultado de la suma es de ${suma}`);
-        alert(`El resultado de la resta es de ${resta}`);
-        alert(`El resultado de la multiplicacion es de ${multi}`);
-        alert(`El resultado de la division es de ${divi}`);
+        let totalPagar= total+(total*iva);
+        if (totalPagar>500000){
+            let totalDesc = totalPagar-(totalPagar*descuento);
+            alert(`El total a pagar es de ${totalDesc}`);
+        } else {
+            alert(`El total a pagar es de ${totalPagar} `);
+        }
     })
 })
