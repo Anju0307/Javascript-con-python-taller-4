991262a300734a3e31ae0f55589a1ef928f5de35
Anju0307 <jpmantilla01@misena.edu.co>
2022-09-29 16:17:38 -0500

    Ejercicio 3

--- main.js
+++ main.js
@@ -2,15 +2,22 @@ addEventListener("DOMContentLoaded", (e)=>{
     let form = document.querySelector("#form");
     form.addEventListener("submit" , (e)=>{
         e.preventDefault();
-        let numero= 3
-        for (let i=1;i<=24;i++){
-            if (i%2==0){
-                numero+=4;
-                alert(numero);
+        let estudiantes = document.querySelector("#estudiantes").value;
+        let notaFinal=0,menor=10,mayor=0;
+        for (let i=1; estudiantes>=i ;i++){
+            let calificacion = parseInt(prompt("Ingrese la calificacion de la cafeteria"));
+            notaFinal= notaFinal+calificacion;
+            if (calificacion>mayor){
+                mayor= calificacion;
+            } else if (calificacion<menor) {
+                menor = calificacion;
             } else{
-                numero-=2;
-                alert(numero);
+                menor= calificacion;
             }
         }
+        let pro = notaFinal/estudiantes;
+        alert(`La calificacion mayor es de ${mayor}`);
+        alert(`La calificacion menor es de ${menor}`);
+        alert(`El promedio es de ${pro}`);
     })
 })
