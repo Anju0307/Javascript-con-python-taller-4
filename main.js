1d0ddc94ddbccb3388a7fc34e8e22a98792b15b2
Anju0307 <jpmantilla01@misena.edu.co>
2022-09-29 17:42:14 -0500

    Ejercicio 7

--- main.js
+++ main.js
@@ -2,18 +2,21 @@ addEventListener("DOMContentLoaded", (e)=>{
     let form = document.querySelector("#form");
     form.addEventListener("submit" , (e)=>{
         e.preventDefault();
-        let trabajadores = document.querySelector("#trabajadores").value;
-        for (let i=1; i<=trabajadores;i++){
-            let tiempo = parseInt(prompt("Ingrese el tiempo que lleva en la empresa"));
-            if (tiempo>0 && tiempo<=5){
-                alert("Se le aumenta 100 pesos");
-            } else if (tiempo >5 && tiempo<=10 ){
-                alert("Se aumentara 250 pesos");
-            } else if (tiempo >10 && tiempo<=20 ){
-                alert("Se aumentara 400 pesos");
-            } else {
-                alert("Se aumentara 550 pesos");
-            }
+        let angulo = document.querySelector("#angulo").value;
+        let serie = document.querySelector("#serie").value;
+        let res=0,num=3,facto=1;
+        for (let i=1; i<=serie;i++){
+            for (let j=1;j<=num;j++){
+                facto *= j; 
+            }   
+            if (i%2==0){
+                res+=(angulo*num)/facto;
+                num+=2;
+            } else{
+                res-=(angulo*num)/facto; 
+                num+=2;
+            }        
         }
+        alert(`El seno del angulo ${angulo} es ${res}`);
     })
 })
